c9d82605923c302c93e2f42c02315a9a63a0c842
amptea <martin.tan31@gmail.com>
2025-07-21 15:28:08 +0800

    Added implementation of leaderboard system, ranking users based on their score

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,18 @@
 import { db } from "@/FirebaseConfig";
 import { useAuth } from "@/hooks/useAuth";
 import { useStatistics } from "@/hooks/useStatistics";
-import { MaterialIcons } from "@expo/vector-icons";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
-import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  updateDoc,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import {
   Alert,
@@ -32,6 +41,8 @@ export default function ProfileScreen() {
   const [score, setScore] = useState(0);
   const [weight, setWeight] = useState(0);
   const [leaderboardModalVisible, setLeaderboardModalVisible] = useState(false);
+  const [topThreeUsers, setTopThreeUsers] = useState<any[]>([]);
+  const [fourthToTenthUsers, setFourthToTenthUsers] = useState<any[]>([]);
 
   useEffect(() => {
     if (!user) return;
@@ -145,7 +156,28 @@ export default function ProfileScreen() {
   };
 
   const handleLeaderboard = () => {
+    getLeaderboardData();
     setLeaderboardModalVisible(true);
+  };
+
+  const getLeaderboardData = async () => {
+    try {
+      const topTenQuery = query(
+        collection(db, "users"),
+        orderBy("score", "desc"),
+        limit(10)
+      );
+      const data = await getDocs(topTenQuery);
+      const leaderboard = data.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setTopThreeUsers(leaderboard.slice(0, 3));
+      setFourthToTenthUsers(leaderboard.slice(3));
+    } catch (error) {
+      Alert.alert("Failed to fetch leaderboard data", "error");
+    }
   };
 
   return (
@@ -397,6 +429,179 @@ export default function ProfileScreen() {
           </View>
         </View>
       </Modal>
+
+      <Modal
+        visible={leaderboardModalVisible}
+        animationType="slide"
+        transparent={false}
+      >
+        <SafeAreaView style={styles.leaderboardContainer}>
+          <View style={styles.leaderboardHeaderRow}>
+            <View style={styles.backButtonIcon}>
+              <TouchableOpacity
+                onPress={() => setLeaderboardModalVisible(false)}
+              >
+                <Ionicons name="chevron-back" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.leaderboardHeaderText}>Leaderboard</Text>
+          </View>
+          {/*First three places UI */}
+          <View style={styles.topThreeContainer}>
+            <View style={styles.runnersUpContainer}>
+              {/* 2nd Place User */}
+              <View
+                style={[
+                  styles.topUsersPictureFrame,
+                  topThreeUsers[1]?.id === user?.uid && {
+                    borderColor: "rgba(62, 104, 35, 1)",
+                    borderWidth: 5,
+                  },
+                ]}
+              >
+                {topThreeUsers[1]?.profilePicture ? (
+                  <Image
+                    source={{ uri: topThreeUsers[1].profilePicture }}
+                    style={styles.topUsersPicture}
+                  />
+                ) : (
+                  <MaterialIcons
+                    name="person"
+                    size={90}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
+                )}
+              </View>
+              <View style={styles.positionContainer}>
+                <Text style={styles.positionText}>2</Text>
+              </View>
+              <View style={styles.userDetails}>
+                <Text style={styles.leaderboardName}>
+                  {topThreeUsers[1]?.name || "Unknown User"}
+                </Text>
+                <Text style={styles.leaderboardPoints}>
+                  {Math.round(topThreeUsers[1]?.score) || "0"} pts
+                </Text>
+              </View>
+            </View>
+            <View style={styles.topUserContainer}>
+              {/* 1st Place User */}
+              <View
+                style={[
+                  styles.topUsersPictureFrame,
+                  topThreeUsers[0]?.id === user?.uid && {
+                    borderColor: "rgba(62, 104, 35, 1)",
+                    borderWidth: 5,
+                  },
+                ]}
+              >
+                {topThreeUsers[0]?.profilePicture ? (
+                  <Image
+                    source={{ uri: topThreeUsers[0].profilePicture }}
+                    style={styles.topUsersPicture}
+                  />
+                ) : (
+                  <MaterialIcons
+                    name="person"
+                    size={90}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
+                )}
+              </View>
+              <View style={styles.positionContainer}>
+                <Text style={styles.positionText}>1</Text>
+              </View>
+
+              <View style={styles.userDetails}>
+                <Text style={styles.leaderboardName}>
+                  {topThreeUsers[0]?.name || "Unknown User"}
+                </Text>
+                <Text style={styles.leaderboardPoints}>
+                  {Math.round(topThreeUsers[0]?.score) || "0"} pts
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.runnersUpContainer}>
+              {/* 3rd Place User */}
+              <View
+                style={[
+                  styles.topUsersPictureFrame,
+                  topThreeUsers[2]?.id === user?.uid && {
+                    borderColor: "rgba(62, 104, 35, 1)",
+                    borderWidth: 5,
+                  },
+                ]}
+              >
+                {topThreeUsers[2]?.profilePicture ? (
+                  <Image
+                    source={{ uri: topThreeUsers[2].profilePicture }}
+                    style={styles.topUsersPicture}
+                  />
+                ) : (
+                  <MaterialIcons
+                    name="person"
+                    size={90}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
+                )}
+              </View>
+              <View style={styles.positionContainer}>
+                <Text style={styles.positionText}>3</Text>
+              </View>
+              <View style={styles.userDetails}>
+                <Text style={styles.leaderboardName}>
+                  {topThreeUsers[2]?.name || "Unknown User"}
+                </Text>
+                <Text style={styles.leaderboardPoints}>
+                  {Math.round(topThreeUsers[2]?.score) || "0"} pts
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          {/* 4th - 10th UI*/}
+          <View style={styles.topTenContainer}>
+            {fourthToTenthUsers.map((userMap, index) => {
+              const isCurrentUser = userMap.id === user?.uid;
+              return (
+                <View
+                  key={userMap.id}
+                  style={[
+                    styles.topTenCard,
+                    isCurrentUser && {
+                      backgroundColor: "rgba(62, 104, 35, 1)",
+                    },
+                  ]}
+                >
+                  <View style={styles.topTenProfileContainer}>
+                    <Text style={styles.topTenNumber}>{index + 4}</Text>
+                    <View style={styles.topTenProfilePicture}>
+                      {userMap?.profilePicture ? (
+                        <Image source={{ uri: userMap.profilePicture }} />
+                      ) : (
+                        <MaterialIcons
+                          name="person"
+                          size={40}
+                          color="rgba(255, 255, 255, 0.6)"
+                        />
+                      )}
+                    </View>
+                  </View>
+                  <View style={styles.topTenDetailsContainer}>
+                    <Text style={styles.topTenText}>
+                      {userMap?.name || "Unknown User"}{" "}
+                    </Text>
+                    <Text style={styles.leaderboardPoints}>
+                      {Math.round(userMap?.score) || "0"} pts
+                    </Text>
+                  </View>
+                </View>
+              );
+            })}
+          </View>
+        </SafeAreaView>
+      </Modal>
     </SafeAreaView>
   );
 }
@@ -428,6 +633,8 @@ const styles = StyleSheet.create({
     width: 64,
     height: 64,
     borderRadius: 32,
+    borderColor: "rgba(200, 200, 200, 0.7)",
+    borderWidth: 1,
   },
   profilePicturePlaceholder: {
     width: 64,
@@ -639,5 +846,127 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginBottom: 8,
     marginLeft: 4,
+  },
+  leaderboardContainer: {
+    flex: 1,
+    backgroundColor: "rgba(47, 52, 55, 1)",
+  },
+  topThreeContainer: {
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "space-between",
+  },
+  runnersUpContainer: {
+    padding: 30,
+  },
+  leaderboardName: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 18,
+    fontWeight: 500,
+  },
+  leaderboardPoints: {
+    color: "rgba(131, 131, 131, 1)",
+  },
+  topUserContainer: {
+    padding: 0,
+  },
+  leaderboardHeaderRow: {
+    position: "relative",
+    flexDirection: "row",
+    paddingTop: 14,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  leaderboardHeaderText: {
+    fontSize: 20,
+    fontWeight: "500",
+    color: "white",
+    marginBottom: 24,
+    fontFamily: "Inter",
+  },
+  backButtonIcon: {
+    position: "absolute",
+    left: 16,
+    paddingTop: 15,
+  },
+  userDetails: {
+    paddingTop: 6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  positionContainer: {
+    marginTop: -8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  positionText: {
+    color: "rgba(255, 255, 255, 1)",
+    backgroundColor: "rgba(255, 154, 2, 0.7)",
+    borderRadius: 50,
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+    fontSize: 14,
+    fontWeight: 500,
+  },
+  topUsersPictureFrame: {
+    width: 90,
+    height: 90,
+    borderRadius: 90,
+    borderColor: "rgba(82, 77, 77, 1)",
+    borderWidth: 1,
+  },
+  topUsersPicture: {
+    width: 81,
+    height: 81,
+    borderRadius: 90,
+  },
+  topTenContainer: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,1)",
+    padding: 10,
+    marginBottom: -40,
+    borderRadius: 30,
+  },
+  topTenCard: {
+    backgroundColor: "rgba(44, 44, 44, 0.9)",
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 12,
+    width: "100%",
+  },
+  topTenProfileContainer: {
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 12,
+  },
+  topTenText: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 17,
+    fontWeight: 500,
+    flex: 1,
+  },
+  topTenProfilePicture: {
+    width: 40,
+    height: 40,
+    borderRadius: 32,
+    borderColor: "rgba(82, 77, 77, 1)",
+    borderWidth: 0.8,
+  },
+  topTenNumber: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 16,
+    fontWeight: 500,
+    width: 26,
+    textAlign: "center",
+    marginRight: 10,
+  },
+  topTenDetailsContainer: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+    paddingRight: 10,
   },
 });
